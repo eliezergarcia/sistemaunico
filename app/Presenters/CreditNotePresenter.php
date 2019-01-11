706892e4f10611cbdb5c7fe900645088425412ff
Eliezer Hernandez <2dcc.eh@gmail.com>
2019-01-11 16:51:47 -0600

    Se corrigio problema en presenter de notas de credito

--- app/Presenters/CreditNotePresenter.php
+++ app/Presenters/CreditNotePresenter.php
@@ -8,7 +8,7 @@ class CreditNotePresenter extends Presenter
 {
     public function statusBadge()
     {
-        if(!$creditnote->deleted_at){
+        if(!$this->model->deleted_at){
             return new HtmlString('<span class="badge badge-success-lighten">Aplicado</span>');
         }else{
             return new HtmlString('<span class="badge badge-danger-lighten">Cancelado</span>');
